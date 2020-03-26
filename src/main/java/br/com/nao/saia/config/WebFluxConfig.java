4467a775d9e5235ab5ea98c2b9466621fbba11fd
isaiastavares <isaiasengsoft@gmail.com>
2020-03-26 20:56:35 -0300

    fix cors

---
+++ src/main/java/br/com/nao/saia/config/WebFluxConfig.java
@@ -0,0 +1,29 @@
+package br.com.nao.saia.config;
+
+import com.sun.org.apache.xerces.internal.parsers.SecurityConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+@Import({SecurityConfiguration.class})
+class ServicesConfiguration implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Methods",
+                        "Access-Control-Allow-Headers",
+                        "Access-Control-Max-Age",
+                        "Access-Control-Request-Headers",
+                        "Access-Control-Request-Method")
+                .maxAge(3600);
+    }
+}
